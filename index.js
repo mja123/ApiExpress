c426a2ca3d19b01483f5c5f30d93f9957584081f
mja123 <matias.aguilar.sansoni@gmail.com>
2021-12-01 09:03:22 -0300

    Adding express and initial express config

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('My App');
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+  res.send("I'm a get method!");
+});
+app.listen(port, () => {
+  console.log(`I'm listening in the port ${port}`);
+});
+
